12725a19872fae77ce4edeb0173fc90165b315bf
Isaac Rodriguez <rilley0189@hotmail.com>
2022-04-19 12:16:28 -0500

    Implementamos la base de datos en memoria para las vistas de Alumnos y Materias

--- Controllers/AlumnoController.cs
+++ Controllers/AlumnoController.cs
@@ -8,32 +8,19 @@ namespace ASP_NET_y_GIT.Controllers
 
         public IActionResult Index()
         {
-            return View(new Alumnos{Nombre="Humanidades IV",
-                                ID=Guid.NewGuid().ToString()});
+            return View(_context.Alumno.FirstOrDefault());
         }
         public IActionResult MultiAlumno()
         {
-            var listaAlumnos = GenerarAlumnos();
             ViewBag.Fecha = DateTime.Now;
 
-            return View("MultiAlumno", listaAlumnos);
+            return View("MultiAlumno", _context.Alumno);
         }
-         private List<Alumnos> GenerarAlumnos()
+         
+        private EscuelaContext _context;
+        public AlumnoController(EscuelaContext context)
         {
-            string[] nombre1 = { "Alondra", "David", "Ricardo", "Itzel", "José", "Aranza", "Isaac", "Isaace" };
-            string[] apellido1 = { "Martinez", "Rodriguez", "Gonzalez", "Aguilar", "Lopez", "Maduro", "Bautista", "Gomez" };
-            string[] apellido2 = { "Ruiz", "Salas", "Corona", "Hernández", "Juarez", "Jiménez", "Ramírez", "Uribe" };
-
-            //Producto cartesiano de los 3 arreglos. Utilizando LinQ para generar este producto
-            //y no utilizar el foreach para la concatenación de los arreglos
-
-            var listaAlumnos = from n1 in nombre1
-                               from a1 in apellido1
-                               from a2 in apellido2
-                               select new Alumnos { Nombre = $"{n1} {a1} {a2}", ID=Guid.NewGuid().ToString()};
-                                                    
-
-            return listaAlumnos.OrderBy((al) => al.ID).Take(30).ToList();
+            _context=context;
         }
     }
 }
--- Controllers/MateriaController.cs
+++ Controllers/MateriaController.cs
@@ -8,41 +8,19 @@ namespace ASP_NET_y_GIT.Controllers
 
         public IActionResult Index()
         {
-            return View(new Materias{Nombre="Humanidades IV",
-                                ID=Guid.NewGuid().ToString()});
+            return View(_context.Materia.FirstOrDefault());
         }
         public IActionResult MultiMateria()
         {
-            var listaMaterias = new List<Materias>
-                {
-                   new Materias{Nombre="Modulación Digital",
-                                ID=Guid.NewGuid().ToString()},
-                   new Materias{Nombre="Teoría de Control Analógico",
-                                ID=Guid.NewGuid().ToString()},
-                   new Materias{Nombre="Sistemas Operativos",
-                                ID=Guid.NewGuid().ToString()},
-                   new Materias{Nombre="Arquitectura de Computadoras",
-                                ID=Guid.NewGuid().ToString()},
-                   new Materias{Nombre="Ingeniería de Software",
-                                ID=Guid.NewGuid().ToString()},
-                   new Materias{Nombre="Met. de la Inv. O Top. Selec de la Ing.I",
-                                ID=Guid.NewGuid().ToString()},
-                   new Materias{Nombre="Probabilidad y Estadística",
-                                ID=Guid.NewGuid().ToString()},
-                   new Materias{Nombre="Análisis de señales Analógicas",
-                                ID=Guid.NewGuid().ToString()},
-                   new Materias{Nombre="Análisis de Algoritmos",
-                                ID=Guid.NewGuid().ToString()},
-                   new Materias{Nombre="Compiladores",
-                                ID=Guid.NewGuid().ToString()},
-                   new Materias{Nombre="Organización de Computadoras",
-                                ID=Guid.NewGuid().ToString()},
-                   new Materias{Nombre="Humanidades IV",
-                                ID=Guid.NewGuid().ToString()}
-                };
+                
             ViewBag.Fecha = DateTime.Now;
 
-            return View("MultiMateria", listaMaterias);
+            return View("MultiMateria", _context.Materia);
+        }
+        private EscuelaContext _context;
+        public MateriaController(EscuelaContext context)
+        {
+            _context=context;
         }
     }
 }
