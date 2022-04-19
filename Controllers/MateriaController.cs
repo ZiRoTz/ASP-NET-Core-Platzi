89894c08a61eca156d109f51cc81f27d303a5b75
Isaac Rodriguez <rilley0189@hotmail.com>
2022-04-18 19:25:07 -0500

    Revert "Generamos la vista de MultiMateria, también se implementó una vista parcial para probar ambas sintaxis"
    This reverts commit e05783676481703f993c3e221bceeb6dfbe963d6.

--- Controllers/MateriaController.cs
+++ Controllers/MateriaController.cs
@@ -5,44 +5,15 @@ namespace ASP_NET_y_GIT.Controllers
 {
     public class MateriaController : Controller
     {
-
         public IActionResult Index()
         {
-            return View(new Materias{Nombre="Humanidades IV",
-                                UniqueID=Guid.NewGuid().ToString()});
-        }
-        public IActionResult MultiMateria()
-        {
-            var listaMaterias = new List<Materias>
-                {
-                   new Materias{Nombre="Modulación Digital",
-                                UniqueID=Guid.NewGuid().ToString()},
-                   new Materias{Nombre="Teoría de Control Analógico",
-                                UniqueID=Guid.NewGuid().ToString()},
-                   new Materias{Nombre="Sistemas Operativos",
-                                UniqueID=Guid.NewGuid().ToString()},
-                   new Materias{Nombre="Arquitectura de Computadoras",
-                                UniqueID=Guid.NewGuid().ToString()},
-                   new Materias{Nombre="Ingeniería de Software",
-                                UniqueID=Guid.NewGuid().ToString()},
-                   new Materias{Nombre="Met. de la Inv. O Top. Selec de la Ing.I",
-                                UniqueID=Guid.NewGuid().ToString()},
-                   new Materias{Nombre="Probabilidad y Estadística",
-                                UniqueID=Guid.NewGuid().ToString()},
-                   new Materias{Nombre="Análisis de señales Analógicas",
-                                UniqueID=Guid.NewGuid().ToString()},
-                   new Materias{Nombre="Análisis de Algoritmos",
-                                UniqueID=Guid.NewGuid().ToString()},
-                   new Materias{Nombre="Compiladores",
-                                UniqueID=Guid.NewGuid().ToString()},
-                   new Materias{Nombre="Organización de Computadoras",
-                                UniqueID=Guid.NewGuid().ToString()},
-                   new Materias{Nombre="Humanidades IV",
-                                UniqueID=Guid.NewGuid().ToString()}
-                };
-            ViewBag.Fecha = DateTime.Now;
+            var materias= new Materias{
+                UniqueID=Guid.NewGuid().ToString(),
+                Nombre="Programación"
+            };
+            ViewBag.Fecha=DateTime.Now;
 
-            return View("MultiMateria", listaMaterias);
+            return View(materias);
         }
     }
 }
