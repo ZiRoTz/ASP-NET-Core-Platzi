b255e88763ee195ed1171b00ea95d9300f1c5e93
Isaac Rodriguez <rilley0189@hotmail.com>
2022-04-20 12:56:24 -0500

    noc

--- Models/Grupos.cs
+++ Models/Grupos.cs
@@ -3,10 +3,10 @@
 namespace ASP_NET_y_GIT.Models;
     public class Grupos:ObjetoEscuelaBase
     {
-        public List<Materias> Materia{ get; set; }
-        public  List<Alumnos> Alumno{ get; set; }
-        public List<Docentes> Docente { get; set; }
-        public List<Links> Link{get; set;}
+        /* public List<Materias> Materia{ get; set; } */
+        /* public  List<Alumnos> Alumno{ get; set; } */
+        /* public List<Docentes> Docente { get; set; } */
+        /* public List<Links> Link{get; set;} */
         public Turnos Turno { get; set; }
         public Semestres Semestre { get; set; }
         public Carreras Carrera { get; set; }
--- Models/Materias.cs
+++ Models/Materias.cs
@@ -2,6 +2,6 @@ namespace ASP_NET_y_GIT.Models;
     public class Materias: ObjetoEscuelaBase
     {
         public List<Docentes> Docente { get; set; }
-        public Grupos Grupos { get; set; }
+        /* public Grupos Grupos { get; set; } */
         public string GruposID { get; set; }
     }
